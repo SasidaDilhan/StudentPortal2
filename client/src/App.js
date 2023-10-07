fad889daf1f5a23c97bcfd66c96b6fd37cdac674
IT21158254 <IT21158254@my.sliit.lk>
2023-10-07 16:41:55 +0530

    app.js created adding routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { component } from "react";
+import { BrowserRouter as Router, Router } from "react-router-dom";
+
+import Navbar from "./components/NavBar";
+import Landing from "./components/Landing";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import FileUpload from "./components/FileUpload";
+import AssignmentList from "./components/AssignmentList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Router exact path="/" component={Landing} />
+        <Router exact path="/register" component={Register} />
+        <Router exact path="/login" component={Login} />
+        <Router exact path="/profile" component={Profile} />
+        <Router exact path="/uploads" component={FileUpload} />
+        <Router exact path="/assignmentlist" component={AssignmentList} />
+      </div>
+    </Router>
   );
 }
-
 export default App;
