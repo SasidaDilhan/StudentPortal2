a6f6df7318e1562ac500d893b5c401ae1eb866f4
IT21158254 <IT21158254@my.sliit.lk>
2023-10-07 17:19:41 +0530

    landing page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { component } from "react";
-import { BrowserRouter as Router, Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Navbar from "./components/NavBar";
 import Landing from "./components/Landing";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Profile from "./components/Profile";
-import FileUpload from "./components/FileUpload";
+//import FileUpload from "./components/FileUpload";
 import AssignmentList from "./components/AssignmentList";
 
 function App() {
@@ -14,12 +14,14 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <Router exact path="/" component={Landing} />
-        <Router exact path="/register" component={Register} />
-        <Router exact path="/login" component={Login} />
-        <Router exact path="/profile" component={Profile} />
-        <Router exact path="/uploads" component={FileUpload} />
-        <Router exact path="/assignmentlist" component={AssignmentList} />
+        {/* <Router exact path="/" component={Landing} />
+        <div className="container">
+          <Router exact path="/register" component={Register} />
+          <Router exact path="/login" component={Login} />
+          <Router exact path="/profile" component={Profile} />
+          <Router exact path="/uploads" component={FileUpload} />
+          <Router exact path="/assignmentlist" component={AssignmentList} />
+        </div> */}
       </div>
     </Router>
   );
---
+++ client/src/components/Landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotorn mt-5">
+          <div className="col-sm-8 mx-auto" align="center">
+            <h1>Student and Instructor Management System</h1>
+            <br />
+            <div align="center" className="col-sm8 mx-auto">
+              <h1>Welcome to student portal</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
